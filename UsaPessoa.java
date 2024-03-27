ea90ceece4cde2b53f903cb2aed8b5e3723575fc
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-03-26 22:33:28 -0300

    Update UsaPessoa.java

--- UsaPessoa.java
+++ UsaPessoa.java
@@ -3,14 +3,20 @@ package br.com.fiap;
 public class UsaPessoa {
 
 	public static void main(String[] args) {
-			
-			Pessoa pessoa1 = new Pessoa();
-			pessoa1.setNome("Astrogildo Planetário");
-			pessoa1.setAnoNascimento(1998);
-			int idade = pessoa1.calculaIdade(2024);
-			System.out.println("Nome: " + pessoa1.getNome());
-			System.out.println("Idade: " + idade + " anos");
-
+		
+		Pessoa pessoa1 = new Pessoa();
+		Pessoa pessoa2 = new Pessoa("Stela", 2015);
+		
+		pessoa1.setNome("David");
+		pessoa1.setAnoNascimento(2005);
+		int idade = pessoa1.calculaIdade(2024);
+		
+		System.out.println("Nome: " + pessoa1.getNome());
+		System.out.println("Idade: " + idade + " anos");
+		
+		System.out.println("\nNome: " + pessoa2.getNome());
+		System.out.println("Idade: " + pessoa2.calculaIdade(2024) + " anos");
+		
 	}
 
 }
