69680aab535a9b1ade4dc79a29fde4545134d81d
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-03-26 22:34:21 -0300

    Update UsaPessoa2.java

--- UsaPessoa2.java
+++ UsaPessoa2.java
@@ -3,23 +3,31 @@ package br.com.fiap;
 import java.util.Scanner;
 
 public class UsaPessoa2 {
-	
-public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		
-		Pessoa pessoa1 = new Pessoa();
+		Pessoa pessoa3;
+		String nome;
+		// int anoNascimento;
 		Scanner scan;
+		
 		try {
 			scan = new Scanner(System.in);
-			System.out.println("Digite seu nome: ");
-			pessoa1.setNome(scan.next());
-			System.out.println("Digite seu ano de nascimento");
-			pessoa1.setAnoNascimento(scan.nextInt());
-			System.out.println("Nome: " + pessoa1.getNome() + "\nIdade: "
-					+ pessoa1.calculaIdade(2024) + " anos");
+			System.out.println("Digite seu nome e ano de nascimento: ");
+			// nome = scan.nextLine();
+			// anoNascimento  = scan.nextInt();
+			// pessoa3 new Pessoa(nome, anoNascimento);
+			
+			pessoa3 = new Pessoa(scan.nextLine(), scan.nextInt());
+			int idade = pessoa3.calculaIdade(2024);
+			
+			System.out.println("Nome: " + pessoa3.getNome());
+			System.out.println("Idade: " + idade + " anos");
 			
 		} catch (Exception e) {
-			System.out.println("Nome e/ou ano de nascimento inváldo");
+			System.out.println("Nome e/ou ano de nascimento incorreto(s)");
 		}
- 
-}
+		
+	}
+
 }
