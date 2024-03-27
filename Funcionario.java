dfb53e74571a4287256aaddeacd6bda0fdd97f08
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-03-26 22:31:58 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -5,22 +5,42 @@ public class Funcionario {
 	private String nome;
 	private float valorHora;
 	
+	public Funcionario() {}
+
+	public Funcionario(String nome, float valorHora) {
+		this.nome = nome;
+		this.valorHora = valorHora;
+	}
+
+	
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public float getValorHora() {
 		return valorHora;
 	}
+
 	public void setValorHora(float valorHora) {
-		this.valorHora = valorHora;
+		try {
+			if (valorHora > 0) {
+				this.valorHora = valorHora;
+			} else {
+				throw new Exception("Valor incorreto! (deve ser maior que 0)");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 	
 	public float salario (float horasSemana) {
 		float salario = valorHora * horasSemana * 4;
 		return salario;
 	}
-	
+
 }
