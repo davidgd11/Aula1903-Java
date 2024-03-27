d8c9990d71f1d75e185ea8318434dbb5f0af73ab
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-03-26 22:32:49 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,25 @@
 package br.com.fiap;
 
+import java.time.LocalDate;
+
 public class Pessoa {
-	
+
+	//atributos
 	private String nome;
 	private int anoNascimento;
 	
+	//construtores
+	//construtor vazio
+	public Pessoa() {}
+	
+	//construtor com passagem de parametros
+	public Pessoa(String nome, int anoNascimento) {
+		this.nome = nome;
+		//this.anoNascimento = anoNascimento;
+		setAnoNascimento(anoNascimento);
+	}
+	
+	//getters & setters
 	public String getNome() {
 		return nome;
 	}
@@ -15,13 +30,22 @@ public class Pessoa {
 		return anoNascimento;
 	}
 	public void setAnoNascimento(int anoNascimento) {
-		this.anoNascimento = anoNascimento;
+		LocalDate anoAtual = LocalDate.now();
+		try {
+			if (anoNascimento >= 1900 && anoNascimento <= anoAtual.getYear()) {
+				this.anoNascimento = anoNascimento;
+			} else {
+				throw new Exception("Valor incorreto! (1900 - ano atual)");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
+	//metodos particulares
 	public int calculaIdade(int anoAtual) {
 		int idade = anoAtual - anoNascimento;
 		return idade;
 	}
+	
 }
-
-
